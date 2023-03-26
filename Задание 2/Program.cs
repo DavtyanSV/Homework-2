2a841241322bdbec66805c3c8f19139017852b33
Сурен Давтян <surendavtan@DavtyanS.local>
2023-03-26 15:37:44 +0300

    Сделаны задачи дз с гитигнор

---
+++ Задание 2/Program.cs
@@ -0,0 +1,33 @@
+﻿System.Console.WriteLine("Введите число:");
+
+var input = int.Parse(Console.ReadLine());
+
+int Solution (int something)
+{
+   something = something%10;
+    return something;
+}
+
+if(input<100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else if(input<1000)
+{
+    System.Console.WriteLine(Solution(input));
+}
+else if (input>1000)
+{
+    while(input>999)
+    {
+        input = input / 10;
+    }
+
+    System.Console.WriteLine(Solution(input));
+
+}
+else
+{
+    System.Console.WriteLine("Какая-то ошибка");
+}
+
---
+++ Задание 3/Program.cs
@@ -0,0 +1,45 @@
+﻿System.Console.WriteLine("Введите день недели:");
+
+bool YesWeekend (int something)
+{
+    if (something >5)
+    {
+       return true;
+        
+    }
+    else
+    {
+       return false;
+       
+    }
+}
+
+bool ItIsWeek (int something)
+{
+    if (something > 7)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int input = int.Parse(Console.ReadLine());
+System.Console.WriteLine("----------------------------------");
+if (ItIsWeek(input))
+{
+    if(YesWeekend(input))
+    {
+        System.Console.WriteLine("Выходной!!!!!!");
+    }
+    else
+    {
+        System.Console.WriteLine("Рабочий день");
+    }
+}
+else
+{
+    System.Console.WriteLine("Ввели некорректное число");
+}
